68c5b53ec398632cfd4706ceb6b958a5a7ee8b69
Xusanov00 <93189344+Xusanov00@users.noreply.github.com>
2023-05-05 09:22:45 +0500

    some small bugs are fixed

--- Notes/Controller/Extensions to Controllers/FSCalendar.swift
+++ Notes/Controller/Extensions to Controllers/FSCalendar.swift
@@ -41,7 +41,7 @@ extension MainViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalend
                     
                 case .noteBlue:
                     switch note.color {
-                    case "noteGreen": eventColorArr.append(.blue)
+                    case "noteBlue": eventColorArr.append(.blue)
                     default: ""
                     }
                 }
@@ -86,7 +86,7 @@ extension MainViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalend
 
                 case .noteBlue:
                     switch note.color {
-                    case "noteGreen": eventColorArr.append(.blue)
+                    case "noteBlue": eventColorArr.append(.blue)
                     default: ""
                     }
                 }
@@ -108,41 +108,56 @@ extension MainViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalend
         tablView.reloadData()
     }
     
+    
+    
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, eventSelectionColorsFor date: Date) -> [UIColor]? {
+        
+        eventColorArr = []
+        
+        for note in DatabaseManager.shared.getNotes() {
+            if date == note.date.toDate()! {
+                switch sortedType {
+                case .all:
+                    switch note.color {
+                    case "noteRed": eventColorArr.append(.red)
+                    case "noteGreen": eventColorArr.append(.green)
+                    case "noteBlue": eventColorArr.append(.blue)
+                    default: ""
+                    }
+                    
+                case .noteRed:
+                    switch note.color {
+                    case "noteRed": eventColorArr.append(.red)
+                    default: ""
+                    }
+                    
+                case .noteGreen:
+                    switch note.color {
+                    case "noteGreen": eventColorArr.append(.green)
+                    default: ""
+                    }
+                    
+                case .noteBlue:
+                    switch note.color {
+                    case "noteBlue": eventColorArr.append(.blue)
+                    default: ""
+                    }
+                }
+                
+            }
+            
+            
+        }
+        // возвращаем цвет точки для этой даты
+        return Array(Set(eventColorArr))
+    }
+    
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, borderSelectionColorFor date: Date) -> UIColor? {
         .red
     }
     
     
-    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleSelectionColorFor date: Date) -> UIColor? {
-        var color: UIColor?
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy"
-
-        switch sortedType {// switches notes by color
-        case .all:
-            return nil
-        case .noteRed:
-            DatabaseManager.shared.getNotes().map { note in
-                if note.date == dateFormatter.string(from: date) && note.color == "noteRed" {
-                    color = .red
-                }
-            }
-        case .noteGreen:
-            DatabaseManager.shared.getNotes().map { note in
-                if note.date == dateFormatter.string(from: date) && note.color == "noteGreen" {
-                    color = .green
-                }
-            }
-        case .noteBlue:
-            DatabaseManager.shared.getNotes().map { note in
-                if note.date == dateFormatter.string(from: date) && note.color == "noteBlue" {
-                    color = .blue
-                }
-            }
-        }
-        print("color is", color)
-        return color ?? nil
-    }
     
     
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillSelectionColorFor date: Date) -> UIColor? {
